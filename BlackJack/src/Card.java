1827a6a7efb73ff7783410e7acee101e5b67758d
Jeka <kotyurginz@mail.ru>
2019-08-01 11:29:07 +0300

    BlackJ

---
+++ BlackJack/src/Card.java
@@ -0,0 +1,35 @@
+public class Card {
+	public String name;
+	public int p;
+	
+	public Card(String n) {
+		name = n;
+	}
+	
+	public String setCard() {
+		return name;
+	}
+	public int getPoints() {
+		return p;
+	}
+	
+	public void calculatePoint() {
+		char[] s = name.toCharArray();
+		
+		if(String.valueOf(s[0]).equals("D") ) {
+			p = 3;
+		}
+		else if(String.valueOf(s[0]).equals("K") ) {
+			p = 4;
+		} 
+		else if(String.valueOf(s[0]).equals("B") ) {
+			p = 2;
+		}
+		else if(String.valueOf(s[0]).equals("T") ) {
+			p = 11;
+		}
+		else {
+			p = Integer.parseInt(String.valueOf(s[0]));
+		}
+	}
+}
---
+++ BlackJack/src/Point.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Point {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Card> cards = new ArrayList<>();
+	    
+		String[] s = {"6♠", "7♠", "8♠", "9♠", "10♠", "B♠", "D♠", "K♠", "T♠","6♣", "7♣", "8♣", "9♣", "10♣", "B♣", "D♣", "K♣", "T♣","6♥", "7♥", "8♥", "9♥", "10♥", "B♥", "D♥", "K♥", "T♥","6♦", "7♦", "8♦", "9♦", "10♦", "B♦", "D♦", "K♦", "T♦"};
+		
+		for(int i = 0;i < s.length; i++ ) {
+			cards.add(new Card(s[i]));
+			cards.get(i).calculatePoint();
+		}
+			
+	
+		System.out.println(cards.get(6).getPoints());
+		System.out.println(cards.get(0).getPoints());
+		   
+		   
+	
+	}
+}
